7d1532288aedf5d5d0773d41454175c24c839ffa
Saul <saul.moro.gomez@gmail.com>
2021-02-14 23:30:43 +0100

    fix(layout): fixing router dependecy in tests

--- src/app/core/layout/main-sidebar/main-sidebar.component.spec.ts
+++ src/app/core/layout/main-sidebar/main-sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MainSidebarComponent } from './main-sidebar.component';
 
@@ -8,6 +9,7 @@ describe('MainSidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [MainSidebarComponent],
     }).compileComponents();
   });
