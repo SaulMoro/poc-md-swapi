fd40dc08195ce3b83aa5082f170b4c80dade1891
Saul <saul.moro.gomez@gmail.com>
2021-02-15 16:32:00 +0100

    fix: fixing tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RoleBasedPreloader, Role, AuthGuard, NoAuthGuard } from './core/auth';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'account',
+    redirectTo: 'starships',
     pathMatch: 'full',
   },
   {
