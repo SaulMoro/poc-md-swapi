8611672f7206022c0dbb8c4639769714679c73e5
Saul <saul.moro.gomez@gmail.com>
2021-02-15 17:02:54 +0100

    test(starships): add mock store to starship list container

--- src/app/features/starships/starships.component.spec.ts
+++ src/app/features/starships/starships.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { StarshipsComponent } from './starships.component';
 
@@ -9,6 +10,7 @@ describe('StarshipsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StarshipsComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
   });
 
