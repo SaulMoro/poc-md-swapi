90447f24c688f425f65b6770bcbb645791f30b8f
Saul <saul.moro.gomez@gmail.com>
2021-02-16 14:09:35 +0100

    test(starships): add starship item list component to starship list test

--- src/app/features/starships/containers/starships-list/starships-list.component.spec.ts
+++ src/app/features/starships/containers/starships-list/starships-list.component.spec.ts
@@ -9,6 +9,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { SharedModule } from '@md-starwars/shared/shared.module';
 import { StarshipsListComponent } from './starships-list.component';
+import { StarshipItemListComponent } from '../../components/starship-item-list/starship-item-list.component';
 
 test('renders the component', async () => {
   await render(StarshipsListComponent, {
@@ -22,6 +23,7 @@ test('renders the component', async () => {
       SharedModule,
     ],
     providers: [provideMockStore()],
+    declarations: [StarshipItemListComponent],
   });
 
   expect(screen).toBeTruthy();
