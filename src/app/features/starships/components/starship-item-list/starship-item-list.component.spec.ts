7822d56c6e53d12cf0c9555d31077a2fb60ea4b2
Saul <saul.moro.gomez@gmail.com>
2021-02-16 14:03:51 +0100

    test(starships): add starship input to item list test

--- src/app/features/starships/components/starship-item-list/starship-item-list.component.spec.ts
+++ src/app/features/starships/components/starship-item-list/starship-item-list.component.spec.ts
@@ -3,6 +3,7 @@ import { SvgIconsModule } from '@ngneat/svg-icon';
 
 import { icons } from '@md-starwars/shared/shared.icons';
 import { LazyImgDirective } from '@md-starwars/shared/directives/lazy-img.directive';
+import { ship } from '@md-starwars/mocks/starships/ship-2.data';
 import { StarshipItemListComponent } from './starship-item-list.component';
 
 test('renders the component', async () => {
@@ -10,6 +11,9 @@ test('renders the component', async () => {
     declarations: [LazyImgDirective],
     imports: [SvgIconsModule.forRoot({ icons })],
     providers: [],
+    componentProperties: {
+      starship: ship,
+    },
   });
 
   expect(screen).toBeTruthy();
